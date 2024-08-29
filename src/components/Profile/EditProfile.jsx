0251d6210971f253fba713b6e3da4f93bb70b44c
SahanDev00 <Dumidus86@gmail.com>
2024-08-29 11:03:48 +0530

    commited

--- src/components/Profile/EditProfile.jsx
+++ src/components/Profile/EditProfile.jsx
@@ -64,11 +64,11 @@ const EditProfile = () => {
             <div className='w-full flex justify-center items-center'>
                 <div className='w-full text-center mt-5'>
                     <form>
-                        <label className='block font-semibold mt-3 text-sm text-white font-poppins'>Current Password</label>
+                        <label className='block font-semibold mt-3 text-sm text-black/80 font-poppins'>Current Password</label>
                         <input className='w-[50%] rounded-lg pl-4 py-1 border-cyan-500 border-2 mb-2 mt-1' type="password" placeholder='••••••••' required />
-                        <label className='block font-semibold mt-3 text-sm text-white font-poppins'>New Password</label>
+                        <label className='block font-semibold mt-3 text-sm text-black/80 font-poppins'>New Password</label>
                         <input className='w-[50%] rounded-lg pl-4 py-1 border-cyan-500 border-2 mb-2 mt-1' type="password" placeholder='••••••••' required />
-                        <label className='font-semibold text-sm mt-3 text-white block font-poppins'>Confirm New Password</label>
+                        <label className='font-semibold text-sm mt-3 text-black/80 block font-poppins'>Confirm New Password</label>
                         <input className='w-[50%] rounded-lg pl-4 py-1 border-cyan-500 border-2 mb-5 mt-1' type="password" placeholder='••••••••' required />
                         <button type='submit' className='block w-[50%] mx-auto mt-2 rounded-lg bg-cyan-500 py-2 text-white font-semibold hover:bg-cyan-600'>Change Password</button>
                     </form>
