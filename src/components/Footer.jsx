2f2c3b0d6fb033ce493dcd81f940ea0731082319
TheSahanD <64869517+SahanDev00@users.noreply.github.com>
2024-11-23 19:38:34 +0530

    Update Footer.jsx
    updated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -41,11 +41,14 @@ const Footer = () => {
         {/* Contact Us Section */}
         <div className='mb-8 md:mb-0'>
           <h2 className='text-xl font-bold text-gray-700 mb-4 font-roboto'>Contact Us</h2>
-          {/*<p className='text-gray-600 mb-1'>
-            Email: info@example.com
-          </p>*/}
+          <p className='text-gray-600 mb-1'>
+            Email: irantha.sandaruwan@gmail.com
+          </p>
           <p className='text-gray-600 mb-1'>
             Phone: 0061403370767
+          </p>
+          <p className='text-gray-600 mb-1'>
+            Phone: 0716616471
           </p>
           <p className='text-gray-600'>
             Sales Office Address: Brisbane
