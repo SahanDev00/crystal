3083c2e31dd9a158e6afabd5e991982770110a7a
SahanDev00 <Dumidus86@gmail.com>
2024-11-27 16:16:39 +0530

    committed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { FaFacebookF, FaTwitter, FaInstagram, FaLinkedinIn } from "react-icons/fa";
+import { FaFacebookF } from "react-icons/fa";
 import {Link} from 'react-router-dom'
 
 const Footer = () => {
